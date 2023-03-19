26ac5772a506c0a60cb062dd4978aff2ff7502c2
Taha Öngören <tahaongoren@Taha-MacBook-Air.local>
2023-03-19 14:21:48 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const containers = document.querySelectorAll('.photo-container');
+
+containers.forEach((container) => {
+  container.addEventListener('click', () => {
+      removeClass();
+    container.classList.add('active');
+  });
+});
+
+const removeClass = () => {
+  containers.forEach((container) => {
+    container.classList.remove('active');
+  });
+};
